b8fd8cacba8062b0044658dd2b148478613362a8
Mateusz Kaleta <m.kaleta9696@gmail.com>
2020-03-03 23:03:06 +0100

    Updates

---
+++ precache-manifest.3e6ae53894a2e58acf082f7f4c01aef2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb8f8d183b1c40485e4e3d5c4b2fa76e",
+    "url": "/todoReact/index.html"
+  },
+  {
+    "revision": "f5b53f7988bd7712551a",
+    "url": "/todoReact/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a260fd0595b47d2d380e",
+    "url": "/todoReact/static/js/2.d768dbdc.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/todoReact/static/js/2.d768dbdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5b53f7988bd7712551a",
+    "url": "/todoReact/static/js/main.a037b536.chunk.js"
+  },
+  {
+    "revision": "d3aea88c07bbca350f4f",
+    "url": "/todoReact/static/js/runtime-main.95541047.js"
+  }
+]);
